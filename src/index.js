03996b479f61c2aec9f1516b89f467556c96f436
Timothy Johnson <timothy@idioticdev.com>
2019-06-15 21:41:13 -0700

    before, after, beforeEach, afterEach

--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ function _test(list, desc, spec) {
     return
   }
 
-  list.push(async () => {
+  const fn = async () => {
     indent(desc)
     await spec(assert)
     list.push(deindent)
-  })
+  }
+
+  fn.isTest = true
+
+  list.push(fn)
 }
 
 let normal = []
@@ -29,6 +33,40 @@ test.skip = function(desc) {
 }
 
 export { test as describe, test as it, test as default }
+
+export function before(fn) {
+  Promise.resolve().then(() => fn(assert))
+}
+
+export function after(fn) {
+  Promise.resolve().then(() =>
+    (only.length ? only : normal).push(() => fn(assert))
+  )
+}
+
+export function beforeEach(fn) {
+  Promise.resolve().then(() => {
+    for (let i = normal.length - 1; i >= 0; i--) {
+      if (normal[i].isTest) normal.splice(i, 0, () => fn(assert))
+    }
+
+    for (let i = only.length - 1; i >= 0; i--) {
+      if (only[i].isTest) only.splice(i, 0, () => fn(assert))
+    }
+  })
+}
+
+export function afterEach(fn) {
+  Promise.resolve().then(() => {
+    for (let i = normal.length - 1; i >= 0; i--) {
+      if (normal[i].isTest) normal.splice(i + 1, 0, () => fn(assert))
+    }
+
+    for (let i = only.length - 1; i >= 0; i--) {
+      if (only[i].isTest) only.splice(i + 1, 0, () => fn(assert))
+    }
+  })
+}
 
 let queue = []
 export async function run() {
